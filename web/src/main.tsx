e1c43f7bf3ee4652d0ca932b92e8fabc601cf282
Thomas Rooney <thomas@speakeasyapi.dev>
2025-01-08 16:03:07 +0000

    chore: limit analytics

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -8,7 +8,11 @@ if (process.env.PUBLIC_POSTHOG_API_KEY) {
   posthog.init(process.env.PUBLIC_POSTHOG_API_KEY, {
     api_host: "https://metrics.speakeasy.com",
     person_profiles: "identified_only",
+    disable_session_recording: true,
+    autocapture: false,
   });
+
+  posthog.capture("$pageview");
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
